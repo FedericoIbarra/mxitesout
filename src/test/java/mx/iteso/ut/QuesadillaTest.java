b0a201f837e8f6367a250e0a15f141d9ecaa940e
Mariana <mari.anasieveg2@gmail.com>
2018-01-31 19:54:12 -0600

    JUnit and Mockito introduction

--- src/test/java/mx/iteso/ut/QuesadillaTest.java
+++ src/test/java/mx/iteso/ut/QuesadillaTest.java
@@ -1,38 +1,49 @@
 package mx.iteso.ut;
 
-import junit.framework.Test;
+import org.junit.Test;
+import org.junit.Before;
+import static org.mockito.Mockito.*;
+
+/*import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
+import org.junit.Before;*/
+
 
 /**
  * Unit test for simple Quesadilla.
- */
-public class QuesadillaTest
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public QuesadillaTest(String testName )
-    {
-        super( testName );
+ *
+ *
+**/
+
+public class QuesadillaTest{
+    @Before
+    public void setUp (){
+        Tortilla tortilla= mock(Tortilla.class);
+        Queso queso = mock(Queso.class);
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( QuesadillaTest.class );
+    @Test
+    public void perfectQuesadilla(){
+        when(tortilla.isToasted()).thenReturn(true);
+        when(queso.isMelted()).thenReturn(true);
     }
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+    @Test
+    public void goodQuesadilla(){
+
+    }
+
+    @Test
+    public void terribleQuesadilla(){
+
+    }
+
+    @Test
+    public void noQuesadilla(){
+
     }
 }
+
+
+
