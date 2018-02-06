75017c8377ba40bb8039777e6e799409a92bbb6f
adriana zarate zarate <adrianazarate0397@gmail.com>
2018-02-06 17:44:53 -0600

    UnitTest con JUnit [01]

--- src/test/java/mx/iteso/ut/QuesadillaTest.java
+++ src/test/java/mx/iteso/ut/QuesadillaTest.java
@@ -1,38 +1,86 @@
 package mx.iteso.ut;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 /**
  * Unit test for simple Quesadilla.
  */
-public class QuesadillaTest
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public QuesadillaTest(String testName )
-    {
-        super( testName );
-    }
+public class QuesadillaTest {
+	
+	Tortilla tortilla;
+	Queso queso;
+	Quesadilla quesadilla = new Quesadilla();
+	
+	@Before
+	public void setUp(){
+		tortilla = mock(Tortilla.class);
+	    queso = mock(Queso.class);
+		quesadilla.setQueso(queso);
+		quesadilla.setTortilla(tortilla);
+	}
+	
+	@Test
+	public void quesadillaPerfecta(){
+		
+		when(tortilla.getToastTemperature()).thenReturn(22);
+		when(tortilla.getCurrentTemperature()).thenReturn(2,6,10,14,18,22);
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( QuesadillaTest.class );
-    }
+		when(queso.getMeltingTemperature()).thenReturn(21);
+		when(queso.getCurrentTemperature()).thenReturn(1,5,9,13,17,21);
+		
+		when(tortilla.isToasted()).thenReturn(true);
+		when(queso.isMelted()).thenReturn(true);
+		
+		Assert.assertEquals("Perfect quesadilla", quesadilla.prepareSingle());
+	}
+	
+	@Test
+	public void quesadillaBuena(){
+		
+		when(tortilla.getToastTemperature()).thenReturn(22);
+		when(tortilla.getCurrentTemperature()).thenReturn(2,6,10,14,18,22);
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+		when(queso.getMeltingTemperature()).thenReturn(21);
+		when(queso.getCurrentTemperature()).thenReturn(1,5,9,13,17,21);
+		
+		when(tortilla.isToasted()).thenReturn(false);
+		when(queso.isMelted()).thenReturn(true);
+		
+		Assert.assertEquals("Good quesadilla", quesadilla.prepareSingle());
+	}
+	
+	@Test
+	public void quesadillaTerrible(){
+		
+		when(tortilla.getToastTemperature()).thenReturn(22);
+		when(tortilla.getCurrentTemperature()).thenReturn(2,6,10,14,18,22);
+
+		when(queso.getMeltingTemperature()).thenReturn(21);
+		when(queso.getCurrentTemperature()).thenReturn(1,5,9,13,17,21);
+		
+		when(tortilla.isToasted()).thenReturn(true);
+		when(queso.isMelted()).thenReturn(false);
+		
+		Assert.assertEquals("Terrible quesadilla", quesadilla.prepareSingle());
+	}
+	
+	@Test
+	public void noHayGas(){
+	
+		when(tortilla.getToastTemperature()).thenReturn(22);
+		when(tortilla.getCurrentTemperature()).thenReturn(2,6,10,14,18,22);
+
+		when(queso.getMeltingTemperature()).thenReturn(21);
+		when(queso.getCurrentTemperature()).thenReturn(1,5,9,13,17,21);
+		
+		when(tortilla.isToasted()).thenReturn(false);
+		when(queso.isMelted()).thenReturn(false);
+		
+		Assert.assertEquals("You ran out of gas", quesadilla.prepareSingle());
+	}
+	
 }
