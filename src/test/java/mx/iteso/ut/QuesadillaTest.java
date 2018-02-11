97283a568dcc2578fbc570dd0b695679c0372d73
EduardoAmaya <is702555@iteso.mx>
2018-02-11 14:17:53 -0600

    pruebas

--- src/test/java/mx/iteso/ut/QuesadillaTest.java
+++ src/test/java/mx/iteso/ut/QuesadillaTest.java
@@ -1,38 +1,48 @@
 package mx.iteso.ut;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
-
+import org.junit.Test;
+import org.junit.Before;
+import static org.mockito.Mockito.*;
 /**
  * Unit test for simple Quesadilla.
  */
-public class QuesadillaTest
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public QuesadillaTest(String testName )
-    {
-        super( testName );
+
+public class  QuesadillaTest{
+
+    @Before
+    public void setUp(){
+        Tortilla tortilla = mock(Tortilla.class);
+        Queso queso = mock(Queso.class);
+        when(tortilla.isToasted()).thenReturn(true);
+        when(tortilla.getCurrentTemperature()).thenReturn(17);
+        Quesadilla quesadilla = new Quesadilla();
+        quesadilla.setQueso(queso);
+        quesadilla.setTortilla(tortilla);
+
+    }
+    @Test
+    public void QuesadillaPerfect(){
+        when(mock.isToasted.thenreturn(true));
+        when(mock.isMelted.thenreturn(true));
+        assert.assertEquals(extends "Perfect Quesadilla", Quesadilla.preparseSingle());
+    }
+    @Test
+    public void QuesaillaCasiPerfecta(){
+        when(mock.isToasted.thenreturn(true));
+        when(mock(isMelted.thenreturn(false));
+        assert.assertEquals(extends "Perfect Quesadilla", Quesadilla.);
+    }
+    @Test
+    public void QuesadillaPromedio(){
+        when(mock.isToasted.thenreturn(false));
+        when(mock.isMelted.thenreturn(true));
+        assert.assertEquals(extends "Quesadilla promedio", quesadilla.preparseSingle());
+    }
+    @Test
+    public void MelacomoFria(){
+        when(mock.isToasted.thenreturn(false));
+        when(mock.isMelted.thenreturn(false));
+        assert.assertEquals(extends "No Gas", quesadilla.preparseSingle());
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( QuesadillaTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
 }
