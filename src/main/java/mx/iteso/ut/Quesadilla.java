432029f90185f3899ffc982852fa32b8f2573c0c
Carmen Martinez <carmen.mtzn@gmail.com>
2018-02-13 11:31:39 -0600

    Quesadilla without errors wooo x3. Checkstyle completed

--- src/main/java/mx/iteso/ut/Quesadilla.java
+++ src/main/java/mx/iteso/ut/Quesadilla.java
@@ -5,57 +5,111 @@ package mx.iteso.ut;
  *
  */
 public class Quesadilla {
+
+    /**
+     * @param cheese af
+     */
     private Queso queso;
+    /**
+     * @param tortilla the tortilla for the quesadilla
+     */
+
     private Tortilla tortilla;
+    /**
+     * @param heatLevel this the heat level (?)
+     */
+
     private int heatLevel;
 
-    public String prepareSingle(){
+    /**
+     *
+     * @return the status of your quesadillita
+     */
+    public final String prepareSingle() {
 
 
-        //If we change the condition || to &&, all the functions will enter to the cycle
+        /*If we change the condition || to &&
+            all the functions will enter to the cycle
+        */
 
-        while(getQueso().getCurrentTemperature()< getQueso().getMeltingTemperature()
-                && getTortilla().getCurrentTemperature()< getTortilla().getToastTemperature()){
-            getTortilla().setCurrentTemperature(getTortilla().getCurrentTemperature() + getHeatLevel());
-            getQueso().setCurrentTemperature(getQueso().getCurrentTemperature() + getHeatLevel());
-            if (getTortilla().getCurrentTemperature() >= getTortilla().getToastTemperature())
+        while (getQueso().getCurrentTemperature()
+                < getQueso().getMeltingTemperature()
+                && getTortilla().getCurrentTemperature()
+                < getTortilla().getToastTemperature()) {
+            getTortilla().setCurrentTemperature(
+                    getTortilla().getCurrentTemperature() + getHeatLevel()
+            );
+            getQueso().setCurrentTemperature(
+                    getQueso().getCurrentTemperature() + getHeatLevel()
+            );
+            if (getTortilla().getCurrentTemperature()
+                    >= getTortilla().getToastTemperature()) {
                 getTortilla().toast(true);
-            if (getQueso().getCurrentTemperature() >= getQueso().getMeltingTemperature())
+            }
+            if (getQueso().getCurrentTemperature()
+                    >= getQueso().getMeltingTemperature()) {
                 getQueso().melt(true);
+            }
         }
-     if(getQueso().isMelted() && getTortilla().isToasted())
+     if (getQueso().isMelted() && getTortilla().isToasted()) {
          return "Perfect quesadilla";
-     if(getQueso().isMelted() && !getTortilla().isToasted())
+     }
+     if (getQueso().isMelted() && !getTortilla().isToasted()) {
          return "Good quesadilla";
-     if(!getQueso().isMelted() && getTortilla().isToasted())
+     }
+     if (!getQueso().isMelted() && getTortilla().isToasted()) {
          return "Terrible quesadilla";
-     else
+     } else {
          return "You ran out of gas";
+     }
 
     }
 
-    public Queso getQueso() {
+    /**
+     *
+     * @return queso instance
+     */
+     final Queso getQueso() {
         return queso;
     }
 
-    public void setQueso(Queso queso) {
+    /**
+     *
+     * @param hQueso set the object queso
+     */
+     final void  setQueso(final Queso hQueso) {
         this.queso = queso;
     }
 
-
-    public Tortilla getTortilla() {
+    /**
+     *
+     * @return the tortilla instance
+     */
+     final Tortilla getTortilla() {
         return tortilla;
     }
 
-    public void setTortilla(Tortilla tortilla) {
+    /**
+     *
+     * @param hTortilla set the tortillita
+     */
+    final void setTortilla(final Tortilla hTortilla) {
         this.tortilla = tortilla;
     }
 
-    public int getHeatLevel() {
+    /**
+     *
+     * @return the heat level
+     */
+     final int getHeatLevel() {
         return heatLevel;
     }
 
-    public void setHeatLevel(int heatLevel) {
+    /**
+     *
+     * @param hHeatLevel set the heat level
+     */
+     final void setHeatLevel(final int hHeatLevel) {
         this.heatLevel = heatLevel;
     }
 }
---
+++ src/main/java/mx/iteso/ut/package-info.java
@@ -0,0 +1,5 @@
+/**
+  package:  mx.iteso.ut this is a package :o.
+ */
+
+package mx.iteso.ut;
